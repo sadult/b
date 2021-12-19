cc6d9efaabe38d98892febf997e8f5d93233652f
Mercad's <info@mercads.ir>
2021-12-19 20:53:37 +0330

    Add files via upload

---
+++ configs/eps.py
@@ -0,0 +1,31 @@
+# Telegram
+TOKEN = '5071788079:AAFRCwJrelNUVZLW19OMmLEIQ-KIfgFLQP0'
+BOT_USERNAME = 'LSTAirdrop_Bot'
+SUDO = 1799474415
+
+# Coin/Token
+NETWORK = 'ethereum'
+TOKEN_NAME = 'Libarty Share Token'
+SYMBOL = 'lst'
+SYMBOL_UPPER = 'LST'
+CONTRACT = '0x355376d6471e09a4ffca8790f50da625630c5270'
+AIRDROP_SUPPLY = '1,000,000'
+REGISTER_REWARD = '1000'
+REFERRAL_REWARD = '200'
+CURRENT_PRICE = '0.054'
+
+# Channels
+CHANNEL1 = 'LST_Token'
+# CHANNEL2 = 'Bull_Premium'
+# CHANNEL3 = 'NPOWithdraw'
+# CHANNEL3 = 'NPODeposit'
+
+# Twitter account's
+TWITTER = 'Libartyshareto1'
+WITHDRAW_OPENING = '20 December'
+
+# Our wallets
+USDT_WALLET = 'TXYEnQGVaXcL3CmtaLRdyd2Q4Y5zXzPGus'
+BNB_WALLET = 'bnb1v9muu5605dk5amzxpnf4tvad4sjuevdwf8peav'
+BUSD_WALLET = '0x3A01428ecA81b945cA8893f334F0D9C7b9890901'
+TRX_WALLET =  'TXYEnQGVaXcL3CmtaLRdyd2Q4Y5zXzPGus'
